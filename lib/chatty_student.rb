35b6c7d54b0d6308c17f88d1dd4de06c9aedf117
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-25 21:52:08 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,7 @@
 class ChattyStudent < Student
   def hello
     super
-    "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   
   def raise_hand
@@ -15,6 +15,6 @@ class ChattyStudent < Student
     super
     super
     super
-   "Pick me!"
+   puts "Pick me!"
   end
 end
